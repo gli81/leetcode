93fb22d417fbf434f66bb45ae416279bee141088
gli81 <liguangzheng998@hotmail.com>
2022-04-29 06:46:18 +0800

    added 037

---
+++ 037SudokuSolver/SudokuSolver.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class SudokuSolver:
+    def solveSudoku(self, board: "list[list[str]]") -> "None":
+        pass
+
+def main():
+    test = SudokuSolver()
+    test1 = []
+    test.solveSudoku(test1)
+    print(test1)
+
+if __name__ == "__main__":
+    main()
+    
