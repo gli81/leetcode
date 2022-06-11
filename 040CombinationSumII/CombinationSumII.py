d1b22b4b28ecd776fae1bf673bd0afce6b47d030
gli81 <liguangzheng998@hotmail.com>
2022-06-12 00:42:30 +0800

    edited 040

--- 040CombinationSumII/CombinationSumII.py
+++ 040CombinationSumII/CombinationSumII.py
@@ -3,7 +3,7 @@
 class CombinationSumII:
     def combinationSum2(self, candidates: "list[int]", target: "int") -> "list[list[int]]":
         ans = []
-        combSum2_(ans)
+        self.combSum2_(ans, candidates, [], target, 0)
         return ans
 
     def combSum2_(self, ans: "list[list[int]]", candidates: "list[int]", current: "list[int]", remain: "int", start: "int"):
@@ -14,6 +14,22 @@ class CombinationSumII:
                 current.append(candidates[i])
                 self.combSum2_(ans, candidates, current, remain - candidates[i], i)
                 current.pop()
+    
+    def rmDup(self, alist: "list[list[int]]") -> "list[list[int]]":
+        ans: "dict[str, str]" = {}
+        for i in range(len(alist)):
+            l = sorted(alist[i])
+            key = ''
+            for j in range(len(l)):
+                key = key + l[j]
+                if j != len(l) - 1:
+                    key += ','
+            ans[key] = ''
+        list_ans = []
+        for item in ans.keys():
+            l = item.split(',')
+            temp = 
+            for num in l:
 
 def main():
     test = CombinationSumII()
