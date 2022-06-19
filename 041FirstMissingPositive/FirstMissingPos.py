c3e48d1fec8575fd66e5c6b8f5e1e6e3f12ad238
gli81 <liguangzheng998@hotmail.com>
2022-06-19 15:15:05 +0800

    edited 041

--- 041FirstMissingPositive/FirstMissingPos.py
+++ 041FirstMissingPositive/FirstMissingPos.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 
+class FirstMissingPos():
+    def firstMissingPos(self, nums: "list[int]"):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
