ec15a9eb6eb36d34b65569f24108a7a4055fadf4
gli81 <liguangzheng998@hotmail.com>
2022-11-17 20:08:13 +0800

    added 051

---
+++ 051NQueens/NQueensI.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class NQueensI():
+    def solveNQueens(self):
+        pass
+
+    def __backtrack(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
