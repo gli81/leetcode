8ae4f640fcf8b6a296a724366c0e5411eb73ecfa
LGZ <liguangzheng998@hotmail.com>
2023-02-17 22:29:17 +0800

    completed 052 py

--- 052NQueensII/NQueensII.py
+++ 052NQueensII/NQueensII.py
@@ -4,11 +4,38 @@ from typing import List
 
 class NQueensII:
     def totalNQueens(self, n: "int") -> "int":
-        pass
+        ans = []
+        self.__backtrack(ans, n, [], 0)
+        return len(ans)
 
-    def __backtrack(self, ):
-        pass
+    def __backtrack(self, ans: "List[List[str]]",
+                    n: "int", pos: "List[int]",
+                    y: "int") -> "None":
+        """
+        pos: records where are the dots
+        """
+        for row in range(n):
+            if self.__is_valid(pos, row, y):
+                ## if (y, row) works,
+                ## add position to the position list
+                pos.append(row)
+                if len(pos) == n:
+                    ## every column has a dot
+                    ans.append(pos[:])
+                    pos.pop()
+                    if n > 1: pos.pop()
+                    return
+                self.__backtrack(ans, n, pos, y+1)
+        if y != 0: pos.pop()
+        return
 
+    def __is_valid(self, pos: "List[int]",
+                    x: "int", y: "int") -> "bool":
+        for i in range(y):
+            if pos[i] == x: return False
+            if pos[i] == x + y - i: return False
+            if pos[i] == x - y + i: return False
+        return True
 
 
 def main():
