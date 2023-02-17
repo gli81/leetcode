a7dcb024b434e074f1932d92aeac9c826a47b8f2
LGZ <liguangzheng998@hotmail.com>
2023-02-17 18:56:42 +0800

    started 052

---
+++ 052NQueensII/NQueensII.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class NQueensII:
+    def totalNQueens(self, n: "int") -> "int":
+        pass
+
+
+def main():
+    test = NQueensII()
+    print(test.totalNQueens(4))
+    print(test.totalNQueens(1))
+    print(test.totalNQueens(3))
+
+if __name__ == "__main__":
+    main()
