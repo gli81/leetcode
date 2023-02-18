1c0c838b7ee3035a0cb14d50909d19d7f6d4e58d
LGZ <liguangzheng998@hotmail.com>
2023-02-19 03:53:14 +0800

    completed 053

--- 053MaximumSubarray/MaximumSubarray.py
+++ 053MaximumSubarray/MaximumSubarray.py
@@ -8,11 +8,11 @@ class MaxSubarray:
         '''
         over time limit, fuck
         '''
-        # dp = [[-sys.maxsize - 1] * len(nums)] * len(nums)
         ans = -sys.maxsize - 1
         n: "int" = len(nums)
-        dp = [[(-sys.maxsize - 1) for i in range(len(nums))]\
-            for _ in range(n)]
+        # dp = [[-sys.maxsize - 1] * len(nums)] * len(nums)
+        dp = [[(-sys.maxsize - 1) for i in range(n)]\
+                                for _ in range(n)]
         for i in range(n):
             dp[i][0] = nums[i]
             if dp[i][0] > ans: ans = dp[i][0]
@@ -23,22 +23,38 @@ class MaxSubarray:
         return ans
     
     def maxSubArray2(self, nums: "List[int]") -> "int":
+        '''
+        with just one array (*list)
+        will not work, just save space =_=
+        '''
         ans = -sys.maxsize - 1
         n: "int" = len(nums)
-        dp = [[(-sys.maxsize - 1) for i in range(len(nums))]\
-            for _ in range(n)]
+        dp = [(-sys.maxsize - 1) for _ in range(n)]
         for i in range(n):
-            dp[i][0] = nums[i]
-            if dp[i][0] > ans: ans = dp[i][0]
+            dp[0] = nums[i]
+            if dp[0] > ans: ans = dp[0]
             for j in range(1, n - i):
-                dp[i][j] = dp[i][j - 1] + nums[j + i]
-                if dp[i][j] > ans: ans = dp[i][j]
+                dp[j] = dp[j - 1] + nums[j + i]
+                if dp[j] > ans: ans = dp[j]
+        return ans
+    
+    def maxSubArray3(self, nums: "List[int]") -> "int":
+        '''
+        AC
+        '''
+        ans = nums[0]
+        n = len(nums)
+        dp = [nums[0] for _ in range(n)]
+        for i in range(1, n):
+            dp[i] = nums[i] if dp[i - 1] <= 0 \
+                            else dp[i - 1] + nums[i]
+            ans = dp[i] if dp[i] > ans else ans
         return ans
 
 
 def main():
     test = MaxSubarray()
-    print(test.maxSubArray2([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    print(test.maxSubArray3([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 
 
 if __name__ == "__main__":
--- 053MaximumSubarray/MaximumSubarrayJava.java
+++ 053MaximumSubarray/MaximumSubarrayJava.java
@@ -2,23 +2,61 @@
 public class MaximumSubarrayJava {
     public static void main(String[] args) {
         MaximumSubarrayJava test = new MaximumSubarrayJava();
-        System.out.println(test.maxSubArray(
+        System.out.println(test.maxSubArray2(
                 new int[]{-2,1,-3,4,-1,2,1,-5,4})
             );
     }
 
-    public int maxSubArray(int[] nums) {
+    /*
+     * over RAM limit :)
+    */
+    public int maxSubArray1(int[] nums) {
+        int ans = Integer.MIN_VALUE;
         int n = nums.length;
-        int[] dp = new int[n];
-        int max = Integer.MIN_VALUE;
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                dp[i] = dp[i] + nums[i + len - 1];
-                if (dp[i] < max) {
-                    max = dp[i];
-                }
+        int[][] dp = new int[n][n];
+        for (int j = 0; j < n; j++) {
+            dp[j][0] = nums[j];
+            if (dp[j][0] > ans) ans = dp[j][0];
+            for (int i = 1; i < n - j; i++) {
+                dp[j][i] = dp[j][i - 1] + nums[j + i];
+                if (dp[j][i] > ans) ans = dp[j][i];
             }
         }
-        return max;
+        return ans;
+    }
+    
+    /*
+     * over time limit :)
+     */
+    public int maxSubArray2(int[] nums) {
+        int ans = Integer.MIN_VALUE;
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int j = 0; j < n; j++) {
+            dp[0] = nums[j];
+            if (dp[0] > ans) ans = dp[0];
+            for (int i = 1; i < n - j; i++) {
+                dp[i] = dp[i - 1] + nums[j + i];
+                if (dp[i] > ans) ans = dp[i];
+            }
+        }
+        return ans;
+    }
+
+    /*
+     * AC
+     */
+    public int maxSubArray3(int[] nums) {
+        int ans = nums[0];
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            // if (dp[i - 1] <= 0) dp[i] = nums[i];
+            // else dp[i] = dp[i - 1] + nums[i];
+            dp[i] = dp[i - 1] <= 0 ? nums[i] : dp[i - 1] + nums[i];
+            if (ans < dp[i]) ans = dp[i];
+        }
+        return ans;
     }
 }
