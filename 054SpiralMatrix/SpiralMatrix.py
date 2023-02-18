f6eb65a18ca49938535dcfec8fd1ceeabddbb779
LGZ <liguangzheng998@hotmail.com>
2023-02-19 03:58:01 +0800

    started 054

---
+++ 054SpiralMatrix/SpiralMatrix.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class SpiralMatrix:
+    def spiralOrder(self,
+           matrix: "List[List[int]]") -> "List[int]":
+        pass
+
+
+def main():
+    test = SpiralMatrix()
+    print(test.spiralOrder())
+
+
+if __name__ == "__main__":
+    main()
