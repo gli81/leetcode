352f748cd45af7b8a5d92f6b4a8da112d9335f6e
LGZ <liguangzheng998@hotmail.com>
2023-02-20 23:04:48 +0800

    Started 055

---
+++ 055JumpGame/JumpGame.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class JumpGame:
+    def canJump(self, nums: "List[int]") -> "bool":
+        pass
+
+
+def main():
+    test = JumpGame()
+    print(test.canJump([2, 3, 1, 1, 4]))
+    print(test.canJump([3, 2, 1, 0, 4]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 055JumpGame/JumpGameJava.java
@@ -0,0 +1,16 @@
+
+public class JumpGameJava{
+    public static void main(String[] args) {
+        JumpGameJava test = new JumpGameJava();
+        System.out.println(
+            test.canJump(new int[]{2, 3, 1, 1, 4})
+        );
+        System.out.println(
+            test.canJump(new int[]{3, 2, 1, 0, 4})
+        );
+    }
+
+    public boolean canJump(int[] nums) {
+        return true;
+    }
+}
