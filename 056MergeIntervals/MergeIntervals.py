39fbabab40723386fd1fd3771438f699134d9f64
LGZ <liguangzheng998@hotmail.com>
2023-02-24 06:16:14 +0800

    Made changes to 056 py

--- 056MergeIntervals/MergeIntervals.py
+++ 056MergeIntervals/MergeIntervals.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 
+from typing import List
+
 class MergeIntervals:
-    def merge(self, intervals: "List[List[int]]")
+    def merge(self, intervals: "List[List[int]]") \
                 -> "List[List[int]]":
         pass
 
@@ -9,7 +11,7 @@ class MergeIntervals:
 def main():
     test = MergeIntervals()
     print(test.merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
-    print(test.merge([[1, 4], [4, 5]))
+    print(test.merge([[1, 4], [4, 5]]))
 
 
 if __name__ == "__main__":
