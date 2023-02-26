1fd27dcbb30f771a355d61c53ce01eba95ac3d6b
LGZ <liguangzheng998@hotmail.com>
2023-02-27 05:22:48 +0800

    Debugging 057 java

--- 057InsertInterval/InsertIntervalJava.java
+++ 057InsertInterval/InsertIntervalJava.java
@@ -1,5 +1,9 @@
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 public class InsertIntervalJava{
     public static void main(String[] args) {
         InsertIntervalJava test = new InsertIntervalJava();
@@ -17,6 +21,44 @@ public class InsertIntervalJava{
 
     public int[][] insert(int[][] intervals,
                             int[] newInterval) {
-        return new int[][]{{0, 0}};
+        // System.out.println(Arrays.toString(newInterval));
+        int start = -1;
+        int end = -1;
+        int new_start = newInterval[0];
+        int new_end = newInterval[1];
+        // why List<int[]> not List<List<Integer>>
+        // https://www.techiedelight.com/zh/convert-int-array-integer-array-java/
+        List<int[]> ans = new ArrayList<>();
+        int index = 0;
+        if (intervals.length == 0) {
+            ans.add(newInterval);
+            return ans.toArray(new int[0][]);
+        }
+        while (index < intervals.length &&
+                intervals[index][1] < new_start) {
+            ans.add(intervals[index++]);
+        }
+        // everything in intervals added
+        // just add newInterval to the end
+        if (index == intervals.length) {
+            start = new_start;
+            end = new_end;
+        } else {
+            start = Math.min(new_start, intervals[index][0]);
+        }
+        while (index < intervals.length && 
+                intervals[index][0] <= new_end) {
+            end = Math.max(
+                Math.max(end, new_end), intervals[index][1]
+            );
+            index++;
+        }
+
+        if (end == -1) end = new_end;
+        ans.add(new int[]{start, end});
+        while (index < intervals.length) {
+            ans.add(intervals[index]);
+        }
+        return ans.toArray(new int[0][]);
     }
 }
