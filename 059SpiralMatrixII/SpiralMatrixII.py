4cd1a2a4aa03ad55757ef513c405e95bdaa76cc2
LGZ <liguangzheng998@hotmail.com>
2023-02-28 09:52:00 +0800

    Worked on 059

--- 059SpiralMatrixII/SpiralMatrixII.py
+++ 059SpiralMatrixII/SpiralMatrixII.py
@@ -4,8 +4,13 @@ from typing import List
 
 class SpiralMatrix:
     def generateMatrix(self, n: "int") -> "List[List[int]]":
+        # if n == 0: return []
         ans = [[0] * n for _ in range(n)]
-        
+        x = y = 0
+        right_border = bottom_border = n
+        left_border = top_border = -1
+        cur_dir = 'l'
+
 
 
 def main():
