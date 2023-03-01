790302842e38c392d69c6c9120dec2e47ca1bf7f
LGZ <liguangzheng998@hotmail.com>
2023-03-01 08:08:53 +0800

    Started 060

---
+++ 060PermutationSequence/PermutationSeq.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+class PermutationSeq:
+    def getPermutation(self, n: "int", k: "int") -> "str":
+        pass
+
+
+def main():
+    test = PermutationSeq()
+    print(test.getPermutation(3, 1))
+    print(test.getPermutation(3, 2))
+    print(test.getPermutation(3, 3))
+    print(test.getPermutation(4, 9))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 060PermutationSequence/PermutationSeqJava.java
@@ -0,0 +1,21 @@
+
+
+public class PermutationSeqJava {
+
+    public String getPermutation(int n, int k) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        PermutationSeqJava test = new PermutationSeqJava();
+        System.out.println(
+            test.getPermutation(3, 1)
+        );
+        System.out.println(
+            test.getPermutation(3, 3)
+        );
+        System.out.println(
+            test.getPermutation(4, 9)
+        );
+    }
+}
