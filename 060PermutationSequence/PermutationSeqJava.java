4f5dc3b5f52fd43d85506017cd090e063e9f6d3b
LGZ <liguangzheng998@hotmail.com>
2023-03-02 16:00:35 +0800

    Completed 060 Java

--- 060PermutationSequence/PermutationSeqJava.java
+++ 060PermutationSequence/PermutationSeqJava.java
@@ -1,9 +1,37 @@
 
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class PermutationSeqJava {
 
     public String getPermutation(int n, int k) {
-        return null;
+        List<Integer> nums = new ArrayList<>();;
+        for (int i = 0; i < n; i++) nums.add(i + 1);
+        return get_ans(nums, n, k);
+    }
+
+    private String get_ans(List<Integer> nums, int n, int k) {
+        if (nums.size() == 1) return nums.get(0) + "";
+        int len_group = factorial(n - 1);
+        int group = (k - 1) / len_group;
+        int num = nums.get(group);
+        nums.remove(group);
+        k = k % len_group;
+        /*
+         * index -1 out of bound
+         * if k = 0 here, in the next recursion
+         * group = -1 / len_group = -1
+         * nums.get(-1) leads to IndexException
+         * so if k = 0, change it to len_group
+         */
+        k = k ==  0 ? len_group : k;
+        return num + get_ans(nums, n - 1, k);
+    }
+
+    private int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
     }
 
     public static void main(String[] args) {
