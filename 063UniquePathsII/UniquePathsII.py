e40618813751bfb3c2f8d474a68d7cbdac0f6b75
LGZ <liguangzheng998@hotmail.com>
2023-03-06 19:31:28 +0800

    Started 063

---
+++ 063UniquePathsII/UniquePathsII.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class UniquePathsII:
+    def uniquePathsWithObstacles(self,
+                        obstacleGrid: "List[List[int]]") -> "int":
+        pass
+
+
+def main():
+    test = UniquePathsII()
+    print(test.uniquePathsWithObstacles())
+
+
+if __name__ == "__main__":
+    main()
---
+++ 063UniquePathsII/UniquePathsIIJava.java
@@ -0,0 +1,10 @@
+
+public class UniquePathsIIJava {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        UniquePathsIIJava test = new UniquePathsIIJava();
+        
+    }
+}
