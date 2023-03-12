8870ac457730a3ea20bafaf381ff8518e4c8099c
LGZ <liguangzheng998@hotmail.com>
2023-03-12 18:34:42 +0800

    Worked on 065

--- 065ValidNum/ValidNum.py
+++ 065ValidNum/ValidNum.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
 class ValidNum:
-    def isNumber(s: "str") -> "bool":
-        pass
+    def isNumber(self, s: "str") -> "bool":
+        if s in ["inf", "-inf", "+inf", "Infinity", "-Infinity", "+Infinity"]: return False
+        try:
+            float(s)
+            return True
+        except:
+            return False
 
+    def isNumber2(self, s: "str" ) -> "bool":
+        pass
 
 def main():
     test = ValidNum()
+    print(test.isNumber('0'))
+    print(test.isNumber('e'))
+    print(test.isNumber('-'))
+
+
+if __name__ == "__main__":
+    main()
