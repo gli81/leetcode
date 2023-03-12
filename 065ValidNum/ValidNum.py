fa36b9f65a03efdc712af34704540eeea176dd6a
LGZ <liguangzheng998@hotmail.com>
2023-03-12 18:49:27 +0800

    Completed 065 py

--- 065ValidNum/ValidNum.py
+++ 065ValidNum/ValidNum.py
@@ -10,13 +10,35 @@ class ValidNum:
             return False
 
     def isNumber2(self, s: "str" ) -> "bool":
-        pass
+        s = s.strip()
+        number = False
+        number_after_e = False
+        point = False
+        e = False
+        for char in range(len(s)):
+            if s[char].isdigit():
+                number = True
+                number_after_e = True
+            elif s[char] in ['e', 'E']:
+                if not number or e:
+                    return False
+                e = True
+                number_after_e = False
+            elif s[char] in ['-', '+']:
+                if char != 0 and s[char - 1] != 'e':
+                    return False
+            elif s[char] == '.':
+                if e or point: return False
+                point = True
+            else: return False
+        return number and number_after_e
 
 def main():
     test = ValidNum()
-    print(test.isNumber('0'))
-    print(test.isNumber('e'))
-    print(test.isNumber('-'))
+    print(test.isNumber2('0'))
+    print(test.isNumber2('e'))
+    print(test.isNumber2('-'))
+    print(test.isNumber2('inf'))
 
 
 if __name__ == "__main__":
