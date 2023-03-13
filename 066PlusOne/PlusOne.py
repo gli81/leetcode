6dc80eff7d5cb3ef9a09b8768aef25f31f9edd0f
LGZ <liguangzheng998@hotmail.com>
2023-03-14 01:38:53 +0800

    Completed 066 Python

--- 066PlusOne/PlusOne.py
+++ 066PlusOne/PlusOne.py
@@ -4,7 +4,17 @@ from typing import List
 
 class PlusOne:
     def plusOne(self, digits: "List[int]") -> "List[int]":
-        pass
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        if digits[0] == 0:
+            digits.insert(0, 1)
+            return digits
+        ## not accessible, just tell compiler to shut up
+        return digits
 
 
 def main():
