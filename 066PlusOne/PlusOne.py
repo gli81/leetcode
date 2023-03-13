56063381fecdb73807532c32c2d5a66b93358a51
LGZ <liguangzheng998@hotmail.com>
2023-03-13 08:11:44 +0800

    Started 066

---
+++ 066PlusOne/PlusOne.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class PlusOne:
+    def plusOne(self, digits: "List[int]") -> "List[int]":
+        pass
+
+
+def main():
+    test = PlusOne()
+    print(test.plusOne([1, 2, 3]))
+    print(test.plusOne([4, 3, 2, 1]))
+    print(test.plusOne([9]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 066PlusOne/PlusOneJava.java
@@ -0,0 +1,13 @@
+
+
+public class PlusOneJava{
+    public int[] plusOne(int[] digits) {
+
+    }
+    public static void main(String[] args) {
+        PlusOneJava test = new PlusOneJava();
+        System.out.println(test.plusOne(new int[]{1, 2, 3}));
+        System.out.println(test.plusOne(new int[]{4, 3, 2, 1}));
+        System.out.println(test.plusOne(new int[]{9}));
+    }
+}
