24efc3124ede6cec1f4093d7be561082f0977dc9
LGZ <liguangzheng998@hotmail.com>
2023-03-14 01:35:05 +0800

    Completed 066 Java

--- 066PlusOne/PlusOneJava.java
+++ 066PlusOne/PlusOneJava.java
@@ -1,13 +1,36 @@
 
 
+import java.util.Arrays;
+
 public class PlusOneJava{
     public int[] plusOne(int[] digits) {
-
+        for (int i = digits.length - 1; i > -1; i--) {
+            if (digits[i] != 9) {
+                digits[i] += 1;
+                return digits;
+            } else digits[i] = 0;
+        }
+        if (digits[0] == 0) {
+            int[] ans = new int[digits.length + 1];
+            ans[0] = 1;
+            // 0 at start, the rest all 0
+            // for (int i = 0; i < digits.length; i++) {
+            //     ans[i + 1] = digits[i]
+            // }
+            return ans;
+        }
+        return new int[]{};
     }
     public static void main(String[] args) {
         PlusOneJava test = new PlusOneJava();
-        System.out.println(test.plusOne(new int[]{1, 2, 3}));
-        System.out.println(test.plusOne(new int[]{4, 3, 2, 1}));
-        System.out.println(test.plusOne(new int[]{9}));
+        System.out.println(
+            Arrays.toString(test.plusOne(new int[]{1, 2, 3}))
+        );
+        System.out.println(
+            Arrays.toString(test.plusOne(new int[]{4, 3, 2, 1}))
+        );
+        System.out.println(
+            Arrays.toString(test.plusOne(new int[]{9}))
+        );
     }
 }
