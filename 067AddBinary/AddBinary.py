f5866431bfe8a217eb9a61f5d00ab7b7ba907030
LGZ <liguangzheng998@hotmail.com>
2023-03-14 21:31:50 +0800

    Started 067

---
+++ 067AddBinary/AddBinary.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class AddBinary:
+    def addBinary(self, a: "str", b: "str") -> "str":
+        pass
+
+
+def main():
+    test = AddBinary()
+    print(test.addBinary("11", "1"))
+    print(test.addBinary("1010", "1011"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 067AddBinary/AddBinaryJava.java
@@ -0,0 +1,13 @@
+
+
+public class AddBinaryJava {
+    public String addBinary(String a, String b) {
+
+    }
+    
+    public static void main(String[] args) {
+        AddBinaryJava test = new AddBinaryJava();
+        System.out.println(test.addBinary("11", "1"));
+        System.out.println(test.addBinary("1010", "1011"));
+    }
+}
