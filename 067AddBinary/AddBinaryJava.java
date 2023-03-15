2d54ce598f59dd0c9d7de3c8179e9163108a0b16
LGZ <liguangzheng998@hotmail.com>
2023-03-16 03:57:59 +0800

    Completed 067 Java

--- 067AddBinary/AddBinaryJava.java
+++ 067AddBinary/AddBinaryJava.java
@@ -27,10 +27,34 @@ public class AddBinaryJava {
     private int bin2Dec2(String bin) {
         return Integer.parseInt(bin, 2);
     }
+
+    public String addBinary2(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        int sum, num1, num2;
+        while (i >= 0 || j >= 0) {
+            num1 = i >= 0 ? a.charAt(i) - '0' : 0;
+            num2 = j >= 0 ? a.charAt(j) - '0' : 0;
+            sum = num1 + num2 + carry;
+            carry = 0;
+            if (sum >= 2) { // 2 or 3
+                carry = 1;
+                sum = sum % 2;
+            }
+            ans.insert(0, sum);
+            i--; j--;
+        }
+        if (carry == 1) ans.insert(0, 1);
+        return ans.toString();
+    }
+
+
     public static void main(String[] args) {
         AddBinaryJava test = new AddBinaryJava();
-        System.out.println(test.addBinary("11", "1"));
-        System.out.println(test.addBinary("1010", "1011"));
+        System.out.println(test.addBinary2("11", "1"));
+        System.out.println(test.addBinary2("1010", "1011"));
         // System.out.println(test.bin2Dec("1011"));
     }
 }
