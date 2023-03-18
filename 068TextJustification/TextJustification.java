889efc6f6211c8a479d9ccc1d81f4530bdef43ee
LGZ <liguangzheng998@hotmail.com>
2023-03-18 08:40:50 +0800

    Worked on 068 Java

--- 068TextJustification/TextJustification.java
+++ 068TextJustification/TextJustification.java
@@ -1,10 +1,33 @@
 
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class TextJustification {
-    public String fullyJustify(String[] words, int maxWidth) {
-        return null;
+    public List<String> fullyJustify(String[] words,
+                                        int maxWidth) {
+        List<String> ans = new ArrayList<>();
+        // current row length
+        int curLen = 0;
+        List<String> row = new ArrayList<>();
+        int i = 0;
+        while (i < words.length) {
+            if (curLen + words[i].length() <= maxWidth) {
+                row.add(words[i]);
+                curLen += words[i].length();
+                curLen += 1;
+            }
+            else {
+                ans.add(String.join(" ", row));
+                row.clear();
+                curLen = 0;
+            }
+            i++;
+        }
+        
+        ans.add(String.join(" ", row));
+        return ans;
     }
 
 
