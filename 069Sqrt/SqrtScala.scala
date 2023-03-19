6281bd06805e18163c71da0692c50ff129ced315
LGZ <liguangzheng998@hotmail.com>
2023-03-20 06:25:22 +0800

    Completed 069 Scala

--- 069Sqrt/SqrtScala.scala
+++ 069Sqrt/SqrtScala.scala
@@ -2,7 +2,21 @@
 
 object SqrtScala {
     def mySqrt(x: Int): Int = {
-        0
+        if (x == 0) return 0
+        if (x == 1) return 1
+        var l: Int = 0
+        var r: Int = x
+        var ans: Int = 0
+        while (l <= r) {
+            var mid: Int = (l + r) / 2
+            var div: Int = x / mid
+            if (div == mid) return mid
+            else if (mid < div) {
+                ans = mid
+                l = mid + 1
+            } else r = mid - 1
+        }
+        ans
     }
 
     def main(args: Array[String]): Unit = {
