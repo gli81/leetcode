01bb1d63c675c29a840e92b77ba18dedd41a5dee
LGZ <liguangzheng998@hotmail.com>
2023-03-22 00:45:13 +0800

    Start 070

--- 070ClimbingStairs/ClimbingStairs.py
+++ 070ClimbingStairs/ClimbingStairs.py
@@ -1,1 +1,16 @@
 # -*- coding: utf -8-*-
+
+class ClimbingStairs:
+    def climbStairs(self, n: "int") -> "int":
+        pass
+
+
+def main():
+    test = ClimbingStairs()
+    print(test.climbStairs(1))
+    print(test.climbStairs(2))
+    print(test.climbStairs(3))
+
+if __name__ == "__main__":
+    main()
+    
