911b3119072e8ffc0dd20dcabd53fa800efbe89a
LGZ <liguangzheng998@hotmail.com>
2023-04-24 05:36:38 +0800

    Completed 075 java

--- 075SortColors/SortColorsJava.java
+++ 075SortColors/SortColorsJava.java
@@ -2,7 +2,20 @@
 
 public class SortColorsJava {
     public void sortColors1(int[] nums) {
-
+        int zero_ct = 0;
+        int one_ct = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) zero_ct++;
+            if (nums[i] == 1) one_ct++;
+        for (i = 0; i < nums.length; i++) {}
+            if (zero_ct > 0) {
+                nums[i] = 0;
+                zero_ct--;
+            } else if (one_ct > 0) {
+                nums[i] = 1;
+                one_ct--;
+            } else nums[i] = 2;
+        }
     }
 
     public static void main(String[] args) {
