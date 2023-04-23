58985e9eda6b6f4bf9b07ef183d64d9cc956ca1e
LGZ <liguangzheng998@hotmail.com>
2023-04-24 01:26:30 +0800

    Completed 074 py

--- 074Searcha2DMatrix/Search2DMatrix.py
+++ 074Searcha2DMatrix/Search2DMatrix.py
@@ -5,7 +5,18 @@ from typing import List
 class Search2DMatrix:
     def searchMatrix1(self, matrix: "List[List[int]]",
                       target: "int") -> "bool":
-        pass
+        flat = []
+        for lst in matrix:
+            flat.extend(lst)
+        left = 0
+        right = len(flat) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if flat[mid] == target: return True
+            elif flat[mid] < target:
+                left = mid + 1
+            else: right = mid - 1
+        return False
 
 
 def main():
