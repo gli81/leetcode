4ebb8b5985c373d2355a89f67d20d3b06c5c7de4
LGZ <liguangzheng998@hotmail.com>
2023-04-24 00:40:42 +0800

    Completed 072 py

--- 072EditDistance/EditDistance.py
+++ 072EditDistance/EditDistance.py
@@ -1,15 +1,29 @@
 # -*- coding: utf -8-*-
 
+from typing import Dict
+
 class EditDistance:
     def minDistance1(self, word1: "str", word2: "str") -> "int":
         pass
 
     def minDistance2(self, word1: "str", word2: "str") -> "int":
-        pass
+        return self.__helper(word1, word2)
 
     def __helper(self, word1: "str", word2: "str",
                 d: "Dict" = {}) -> "int":
-        pass
+        if len(word1) == 0 and len(word2) == 0: return 0
+        if len(word1) == 0: return len(word2)
+        if len(word2) == 0: return len(word1)
+        key = word1 + '_' + word2
+        if key in d: return d[key]
+        else:
+            x = self.__helper(word1, word2[:-1], d) + 1
+            y = self.__helper(word1[:-1], word2, d) + 1
+            z = self.__helper(word1[:-1], word2[:-1], d)
+            if word1[-1] != word2[-1]: z += 1
+            d[key] = min(x, y, z)
+            return min(x, y, z)
+
 
 
 def main():
