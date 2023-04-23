aab2e9a677315e6fbc7364fc0f92d3858887d93e
LGZ <liguangzheng998@hotmail.com>
2023-04-24 05:49:58 +0800

    Started 076

---
+++ 076MinWindowSubstring/MinWindowSubstring.py
@@ -0,0 +1,12 @@
+# -*- coding: utf -8-*-
+
+class MinWindowSubstring:
+    def minWindow(self, s: "str", t: "str") -> "str":
+        pass
+
+
+def main():
+    test = MinWindowSubstring()
+    print(test.minWindow("ADOBECODEBANC", "ABC"))
+    print(test.minWindow("a", "a"))
+    print(test.minWindow("a", "aa"))
---
+++ 076MinWindowSubstring/MinWindowSubstringJava.java
@@ -0,0 +1,14 @@
+
+
+public class MinWindowSubstringJava {
+    public String minWindow(String s, String t) {
+        return null;
+    }
+    public static void main(String[] args) {
+        MinWindowSubstringJava test = new MinWindowSubstringJava();
+        System.out.println(test.minWindow("ADOBECODEBANC",
+                                            "ABC"));
+        System.out.println(test.minWindow("a", "a"));
+        System.out.println(test.minWindow("a", "aa"));
+    }    
+}
