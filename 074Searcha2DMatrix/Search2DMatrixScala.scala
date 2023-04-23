3497c0469915c83fc43ada50c3ac55f130fc6232
LGZ <liguangzheng998@hotmail.com>
2023-04-24 03:30:20 +0800

    Completed 074

--- 074Searcha2DMatrix/Search2DMatrixScala.scala
+++ 074Searcha2DMatrix/Search2DMatrixScala.scala
@@ -11,7 +11,7 @@ object Search2DMatrixScala {
             else if (flat(mid) < target) left = mid + 1
             else right = mid - 1
         }
-        return false
+        false
     }
 
     def main(args: Array[String]): Unit = {
