0b89a81abc82e64b3a4e00ce4f349c8e8bd5903c
LGZ <liguangzheng998@hotmail.com>
2023-04-24 03:29:19 +0800

    Completed 075

--- 074Searcha2DMatrix/Search2DMatrixScala.scala
+++ 074Searcha2DMatrix/Search2DMatrixScala.scala
@@ -1,6 +1,33 @@
 object Search2DMatrixScala {
     def searchMatrix(matrix: Array[Array[Int]],
                     target: Int): Boolean = {
-        
+        val flat: Array[Int] = matrix.flatten
+        // println(flat.mkString(","))
+        var left: Int = 0
+        var right: Int = flat.length - 1;
+        while (left <= right) {
+            var mid : Int = (left + right) / 2
+            if (flat(mid) == target) return true
+            else if (flat(mid) < target) left = mid + 1
+            else right = mid - 1
+        }
+        return false
+    }
+
+    def main(args: Array[String]): Unit = {
+        println(searchMatrix(
+            Array(
+                Array(1, 3, 5, 7),
+                Array(10, 11, 16, 20),
+                Array(23, 30, 34, 50)
+            ), 3
+        ))
+        println(searchMatrix(
+            Array(
+                Array(1, 3, 5, 7),
+                Array(10, 11, 16, 20),
+                Array(23, 30, 34, 50)
+            ), 13
+        ))
     }
 }
