d03b107bb0ffd8b7bbcbafb3903d155053232075
LGZ <liguangzheng998@hotmail.com>
2023-04-24 14:54:40 +0800

    Worked on 076

--- 076MinWindowSubstring/MinWindowSubstringJava.java
+++ 076MinWindowSubstring/MinWindowSubstringJava.java
@@ -2,6 +2,12 @@
 
 public class MinWindowSubstringJava {
     public String minWindow(String s, String t) {
+        int left = 0;
+        int right = 0;
+        int ans_left = 0;
+        int ans_right = -1;
+        int ans_len = Integer.MAX_VALUE;
+        
         return null;
     }
     public static void main(String[] args) {
