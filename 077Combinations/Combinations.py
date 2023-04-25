4fe0f2265eda6725f1f9dc1e66f38922c5dd4ad5
LGZ <liguangzheng998@hotmail.com>
2023-04-25 15:25:00 +0800

    Completed 077 py

--- 077Combinations/Combinations.py
+++ 077Combinations/Combinations.py
@@ -1,14 +1,29 @@
 # -*- coding: utf -8-*-
 
+from typing import List
+
 class Combinations:
-    def combine(self, n: "int", k: "int"):
-        pass
+    def combine1(self, n: "int", k: "int"):
+        ans = []
+        self.__get_ans(n, k, ans, [], 1)
+        return ans
+
+    def __get_ans(self, n: "int", k: "int",
+                  ans: "List[List[int]]", cur: "List[int]",
+                  start: "int") -> "None":
+        if len(cur) == k:
+            ans.append(cur[:])
+            return
+        for i in range(start, n + 1):
+            cur.append(i)
+            self.__get_ans(n, k, ans, cur, i + 1)
+            cur.pop()
 
 
 def main():
     test = Combinations()
-    print(test.combine(4, 2))
-    print(test.combine(1, 1))
+    print(test.combine1(4, 2))
+    print(test.combine1(1, 1))
 
 if __name__ == "__main__":
     main()
--- 077Combinations/CombinationsJava.java
+++ 077Combinations/CombinationsJava.java
@@ -1,10 +1,15 @@
 
 
-public class CombinationsJava {
+import java.util.List;
 
+public class CombinationsJava {
+    public List<List<Integer>> combine1(int n, int k) {
+        return null;
+    }
     
     public static void main(String[] args) {
         CombinationsJava test = new CombinationsJava();
-        System.out.println();
+        System.out.println(test.combine1(4, 2));
+        System.out.println(test.combine1(1, 1));
     }
 }
