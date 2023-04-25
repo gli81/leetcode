7b6a1f76d36686198072374e5b2f8c9abbca81e6
LGZ <liguangzheng998@hotmail.com>
2023-04-25 09:25:31 +0800

    Started 077

---
+++ 077Combinations/Combinations.py
@@ -0,0 +1,14 @@
+# -*- coding: utf -8-*-
+
+class Combinations:
+    def combine(self, n: "int", k: "int"):
+        pass
+
+
+def main():
+    test = Combinations()
+    print(test.combine(4, 2))
+    print(test.combine(1, 1))
+
+if __name__ == "__main__":
+    main()
---
+++ 077Combinations/CombinationsJava.java
@@ -0,0 +1,10 @@
+
+
+public class CombinationsJava {
+
+    
+    public static void main(String[] args) {
+        CombinationsJava test = new CombinationsJava();
+        System.out.println();
+    }
+}
