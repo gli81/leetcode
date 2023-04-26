196c2ea0dfe79d62b0b063d561f0142ec90ec65a
LGZ <liguangzheng998@hotmail.com>
2023-04-26 19:52:20 +0800

    Completed 079

--- 079WordSearch/WordSearch.py
+++ 079WordSearch/WordSearch.py
@@ -3,8 +3,50 @@
 from typing import List
 
 class WordSearch:
-    def exist(self, board: "List[List[str]]", word: "str") -> "bool":
-        pass
+    def exist(self, board: "List[List[str]]",
+              word: "str") -> "bool":
+        '''
+        over time limit, fuck it
+        '''
+        rows = len(board)
+        if not rows: return False
+        cols = len(board[0])
+        used = [[False] * cols for _ in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
+                if self.__exist_(board, word, i, j,
+                                 0, used): return True
+        return False
+
+    def __exist_(self, board: "List[List[str]]", word: "str",
+                 row: "int", col: "int", index: "int",
+                 used: "List[List[bool]]") -> "bool":
+        ## out of bound
+        if row not in range(0, len(board)) or\
+              col not in range(0, len(board[0])): return False
+        ## used
+        if used[row][col]: return False
+        ## not what we are looking for
+        if board[row][col] != word[index]: return False
+        ## found the entire thing
+        if index == len(word) - 1: return True
+        used[row][col] = True
+        ## try four directions
+        ## up
+        if self.__exist_(board, word, row - 1, col,
+                         index + 1, used): return True
+        ## down
+        if self.__exist_(board, word, row + 1, col,
+                         index + 1, used): return True
+        ## left
+        if self.__exist_(board, word, row, col - 1,
+                         index + 1, used): return True
+        ## right
+        if self.__exist_(board, word, row, col + 1,
+                         index + 1, used): return True
+        ## nothing works
+        used[row][col] = False
+        return False
 
 
 def main():
--- 079WordSearch/WordSearchJava.java
+++ 079WordSearch/WordSearchJava.java
@@ -6,13 +6,48 @@ public class WordSearchJava {
         if (rows == 0) return false;
         int cols = board[0].length;
         boolean[][] visited = new boolean[rows][cols];
-        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (exist_(board, i, j, word, 0, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private boolean exist_(char[][] board, int row, int col,
                             String word, int index,
                             boolean[][] visited) {
-        
+        // out of board
+        if (row < 0 || row >= board.length ||
+            col < 0 || col >= board[0].length) {
+            return false;
+        }
+        // visited
+        // not what we are looking for
+        if (visited[row][col] ||
+            board[row][col] != word.charAt(index)) {
+            return false;
+        }
+        if (index == word.length() - 1) return true;
+        visited[row][col] = true;
+        // try four directions
+        // up
+        if (exist_(board, row - 1, col,
+                    word, index + 1, visited)) return true;
+        // down
+        if (exist_(board, row + 1, col,
+                    word, index + 1, visited)) return true;
+        // left
+        if (exist_(board, row, col - 1,
+                    word, index + 1, visited)) return true;
+        // right
+        if (exist_(board, row, col + 1,
+                    word, index + 1, visited)) return true;
+        // no next letter
+        visited[row][col] = false;
+        return false;
     }
 
     public static void main(String[] args) {
