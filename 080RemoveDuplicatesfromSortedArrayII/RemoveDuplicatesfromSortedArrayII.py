5f8b430788686c40e58c3246a4b22fd853d6904f
LGZ <liguangzheng998@hotmail.com>
2023-04-27 00:04:19 +0800

    Reformat & Started 080

---
+++ 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.py
@@ -0,0 +1,16 @@
+# -*- coding: utf -8-*-
+
+from typing import List
+
+class RemoveDuplicatesfromSortedArrayII:
+    def removeDuplicates(self, nums: "List[int]") -> "int":
+        pass
+
+
+def main():
+    test = RemoveDuplicatesfromSortedArrayII()
+    print(test.removeDuplicates([1, 1, 1, 2, 2, 3]))
+    print(test.removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+
+if __name__ == "__main__":
+    main()
---
+++ 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayIIJava.java
@@ -0,0 +1,18 @@
+
+
+public class RemoveDuplicatesfromSortedArrayIIJava {
+    public int removeDuplicates(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesfromSortedArrayIIJava test = 
+        new RemoveDuplicatesfromSortedArrayIIJava();
+        System.out.println(test.removeDuplicates(
+            new int[]{1, 1, 1, 2, 2, 3}
+        ));
+        System.out.println(test.removeDuplicates(
+            new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3}
+        ));
+    }
+}
