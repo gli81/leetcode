9016adfe1f1cf81e1e649c3291a72494eca8e59f
LGZ <liguangzheng998@hotmail.com>
2023-04-28 16:11:25 +0800

    Completed 080 py

--- 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.py
+++ 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.py
@@ -4,7 +4,22 @@ from typing import List
 
 class RemoveDuplicatesfromSortedArrayII:
     def removeDuplicates(self, nums: "List[int]") -> "int":
-        pass
+        slow = 0
+        fast = 1
+        ct = 1
+        max_ = 2
+        while fast < len(nums):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+                ct = 1
+            else:
+                if ct < max_:
+                    slow += 1
+                    nums[slow] = nums[fast]
+                    ct += 1
+            fast += 1
+        return slow + 1
 
 
 def main():
