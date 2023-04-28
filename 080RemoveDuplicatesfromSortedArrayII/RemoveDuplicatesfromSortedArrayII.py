35883058a0318e33a311ecb42a01ee51f7a891ca
LGZ <liguangzheng998@hotmail.com>
2023-04-28 16:20:27 +0800

    Completed 080. cu in may :-)

--- 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.py
+++ 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.py
@@ -3,29 +3,36 @@
 from typing import List
 
 class RemoveDuplicatesfromSortedArrayII:
-    def removeDuplicates(self, nums: "List[int]") -> "int":
+    def removeDuplicates1(self, nums: "List[int]") -> "int":
         slow = 0
         fast = 1
         ct = 1
-        max_ = 2
         while fast < len(nums):
             if nums[fast] != nums[slow]:
                 slow += 1
                 nums[slow] = nums[fast]
                 ct = 1
             else:
-                if ct < max_:
+                if ct == 1:
                     slow += 1
                     nums[slow] = nums[fast]
                     ct += 1
             fast += 1
         return slow + 1
-
+    
+    def removeDuplicates2(self, nums: "List[int]") -> "int":
+        i = 0
+        for n in nums:
+            if i < 2 or n > nums[i-2]:
+                nums[i] = n
+                i += 1
+        return i
+    
 
 def main():
     test = RemoveDuplicatesfromSortedArrayII()
-    print(test.removeDuplicates([1, 1, 1, 2, 2, 3]))
-    print(test.removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+    print(test.removeDuplicates2([1, 1, 1, 2, 2, 3]))
+    print(test.removeDuplicates2([0, 0, 1, 1, 1, 1, 2, 3, 3]))
 
 if __name__ == "__main__":
     main()
--- 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayIIJava.java
+++ 080RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayIIJava.java
@@ -2,7 +2,22 @@
 
 public class RemoveDuplicatesfromSortedArrayIIJava {
     public int removeDuplicates(int[] nums) {
-        return 0;
+        int slow = 0;
+        int fast = 1;
+        int ct = 1;
+        while (fast < nums.length) {
+            if (nums[slow] == nums[fast]) {
+                if (ct == 1) {
+                    ct++;
+                    nums[++slow] = nums[fast];
+                }
+            } else {
+                ct = 1;
+                nums[++slow] = nums[fast];
+            }
+            fast++;
+        }
+        return ++slow;
     }
 
     public static void main(String[] args) {
