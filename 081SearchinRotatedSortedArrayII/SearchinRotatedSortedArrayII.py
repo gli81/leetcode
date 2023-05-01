a68c1abaedd97a0171f46df56ee10e3eed9c0bcf
LGZ <liguangzheng998@hotmail.com>
2023-05-01 15:42:37 +0800

    Started 081

---
+++ 081SearchinRotatedSortedArrayII/SearchinRotatedSortedArrayII.py
@@ -0,0 +1,12 @@
+# -*- coding: utf -8-*-
+
+from typing_extensions import List
+
+class SearchinRotatedSortedArrayII:
+    def search(self, nums: "List[int]", target: "int") -> "bool":
+        pass
+
+
+def main():
+    test = SearchinRotatedSortedArrayII()
+    print(test.search())
