37988bc88ec8284a0eeb50190eab77590907a6df
LGZ <liguangzheng998@hotmail.com>
2023-05-03 01:12:46 +0800

    Worked on 082

--- 082RemoveDuplicatesfromSortedListII/RemoveDuplicatesfromSortedListII.py
+++ 082RemoveDuplicatesfromSortedListII/RemoveDuplicatesfromSortedListII.py
@@ -1,17 +1,60 @@
 # -*- coding: utf -8-*-
 
-class RemoveDuplicatesfromSortedListII:
-    def deleteDuplicates(self, head: "ListNode"):
-        pass
+from typing import Optional
 
+class RemoveDuplicatesfromSortedListII:
+    def deleteDuplicates(self, head: "ListNode") -> "Optional[ListNode]":
+        print(f"head: {head}")
+        cur = head
+        pre = ListNode(0, head)
+        while cur and cur.next:
+            eq = False
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next
+                eq = True
+            if eq:
+                ## a series of duplicates
+                ## pre.next = cur.next
+                pre.next = cur.next
+            else:
+                pre = cur
+            cur = cur.next
+            # print(head)
+        return pre.next
+
+        
 
 class ListNode:
     def __init__(self, val = 0, next = None):
         self.val = val
         self.next = next
+    
+    def __str__(self):
+        ans = '['
+        ans += str(self.val)
+        cur = self
+        while cur.next:
+            cur = cur.next
+            ans = ans + ", " + str(cur.val)
+        ans += ']'
+        return ans
 
 def main():
-    pass
+    test = RemoveDuplicatesfromSortedListII()
+    print(test.deleteDuplicates(ListNode(1,
+                                ListNode(2,
+                                ListNode(3,
+                                ListNode(3,
+                                ListNode(4,
+                                ListNode(4,
+                                ListNode(5)))))))))
+    print(test.deleteDuplicates(ListNode(1,
+                                ListNode(1,
+                                ListNode(3,
+                                ListNode(3,
+                                ListNode(4,
+                                ListNode(4,
+                                ListNode(5)))))))))
 
 if __name__ == "__main__":
     main()
