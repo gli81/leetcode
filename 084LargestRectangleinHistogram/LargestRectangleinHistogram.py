3c7e3dea8332bf4621fa95816b4c1931f25cbe4d
LGZ <liguangzheng998@hotmail.com>
2023-05-03 03:48:21 +0800

    Started 084

---
+++ 084LargestRectangleinHistogram/LargestRectangleinHistogram.py
@@ -0,0 +1,16 @@
+# -*- coding: utf -8-*-
+
+from typing import List
+
+class LargestRectangleinHistogram:
+    def largestRectangleArea(self, heights: "List[int]") -> "int":
+        pass
+
+
+def main():
+    test = LargestRectangleinHistogram()
+    print(test.largestRectangleArea([2, 1, 5, 6, 2, 3]))
+    print(test.largestRectangleArea([2, 4]))
+
+if __name__ == "__main__":
+    main()
---
+++ 084LargestRectangleinHistogram/LargestRectangleinHistogramJava.java
@@ -0,0 +1,13 @@
+
+
+public class LargestRectangleinHistogramJava {
+    public int largestRectangleArea(int[] heights) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        LargestRectangleinHistogramJava test =
+            new LargestRectangleinHistogramJava();
+        System.out.println(test);
+    }
+}
