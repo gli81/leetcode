589b3bdffdf891f6bd9870469c8dac4af8f2d835
LGZ <liguangzheng998@hotmail.com>
2023-05-10 00:19:23 +0800

    Completed 084 py

--- 084LargestRectangleinHistogram/LargestRectangleinHistogram.py
+++ 084LargestRectangleinHistogram/LargestRectangleinHistogram.py
@@ -37,20 +37,35 @@ class LargestRectangleinHistogram:
         len_ = len(heights)
         ## right_min[i]存heights中heights[i]右边
         ## 第一个比heights[i]小的
-        right_min = [-1] * len_
-        left_min = [-1] * len_
-        ## TODO left min
+        left_first_shorter = [_ - 1 for _ in range(len_)]
+        right_first_shorter = [_ + 1 for _ in range(len_)]
+        ## left min
+        ## intuitive way
+        # for i in range(1, len_):
+        #     p = i - 1
+        #     while p >= 0 and heights[p] >= heights[i]:
+        #         p -= 1
+        #     left_min[i] = p
         for i in range(1, len_):
             p = i - 1
             while p >= 0 and heights[p] >= heights[i]:
-                p -= 1
-            left_min[i] = p
+                ## ?????
+                p = left_first_shorter[p]
+            left_first_shorter[i] = p
+        # print(left_first_shorter)
 
-        ## TODO right min
+        ## right min
+        ## start from right, or infinite loop
+        for i in range(len_ - 2, -1, -1):
+            p = i + 1
+            while p < len_ and heights[p] >= heights[i]:
+                p = right_first_shorter[p]
+            right_first_shorter[i] = p
+        # print(right_first_shorter)
 
         max_area = 0
         for i in range(len_):
-            area = (right_min[i] - left_min[i] - 1) * heights[i]
+            area = (right_first_shorter[i] - left_first_shorter[i] - 1) * heights[i]
             max_area = max(area, max_area)
         return max_area
 
