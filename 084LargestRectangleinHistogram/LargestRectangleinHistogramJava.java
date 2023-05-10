91b61cef501f4ee81cfdcb48a6ca86e97fa9d5b2
LGZ <liguangzheng998@hotmail.com>
2023-05-10 12:05:14 +0800

    Completed 084 'left right method' java

--- 084LargestRectangleinHistogram/LargestRectangleinHistogramJava.java
+++ 084LargestRectangleinHistogram/LargestRectangleinHistogramJava.java
@@ -1,5 +1,6 @@
 
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class LargestRectangleinHistogramJava {
@@ -32,7 +33,48 @@ public class LargestRectangleinHistogramJava {
 
 
     public int largestRectangleArea4(int[] heights) {
-        return 0;
+        /**
+         * find index of first shorter hist 
+         * to the left of heights[i] => left_first_shorter
+         * 
+         * and index of first shorter hist to the right of
+         * heights[i] => right_first_shorter
+         */
+        int len_ = heights.length;
+        int[] left_first_shorter = new int[len_];
+        int[] right_first_shorter = new int[len_];
+        for (int i = 0; i < len_; i++) {
+            left_first_shorter[i] = i - 1;
+            right_first_shorter[i] = i + 1;
+        }
+        // 
+        for (int i = 1; i < len_; i++) {
+            int l = i - 1;
+            while (l >= 0 && heights[l] >= heights[i]) {
+                l = left_first_shorter[l];
+            }
+            left_first_shorter[i] = l;
+        }
+        // System.out.println(Arrays.toString(left_first_shorter));
+        for (int i = len_ - 2; i > -1; i--) {
+            int r = i + 1;
+            while (r < len_ && heights[r] >= heights[i]) {
+                r = right_first_shorter[r];
+            }
+            right_first_shorter[i] = r;
+        }
+        // System.out.println(Arrays.toString(right_first_shorter));
+
+        // get max area
+        int max_area = 0;
+        int area = 0;
+        for (int i = 0; i < len_; i++) {
+            area = heights[i] * (
+                right_first_shorter[i] - left_first_shorter[i] - 1
+            );
+            max_area = Math.max(area, max_area);
+        }
+        return max_area;
     }
     
     public static void main(String[] args) {
