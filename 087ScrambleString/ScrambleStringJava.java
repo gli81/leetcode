65e7cf6cb863648feee3d1aef938059aac3e0308
LGZ <liguangzheng998@hotmail.com>
2023-05-13 05:01:22 +0800

    worked on 087 java

--- 087ScrambleString/ScrambleStringJava.java
+++ 087ScrambleString/ScrambleStringJava.java
@@ -2,6 +2,18 @@
 
 public class ScrambleStringJava {
     public boolean isScramble(String s1, String s2) {
+        // special cases
+        if (s1.length() != s2.length()) return false;
+        if (s1.equals(s2)) return true;
+        int [] letters = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            letters[s1.charAt(i) - 'a']++;
+            letters[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] != 0) return false;
+        }
+
         return false;
     }
     
