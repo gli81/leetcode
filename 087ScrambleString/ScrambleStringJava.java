5149800d2c8d5c39bef9d0c78a20d459b74e6bf0
LGZ <liguangzheng998@hotmail.com>
2023-05-13 16:50:26 +0800

    Completed 087 Java

--- 087ScrambleString/ScrambleStringJava.java
+++ 087ScrambleString/ScrambleStringJava.java
@@ -2,7 +2,7 @@
 
 public class ScrambleStringJava {
     public boolean isScramble(String s1, String s2) {
-        // special cases
+        // special cases; exiting conditions
         if (s1.length() != s2.length()) return false;
         if (s1.equals(s2)) return true;
         int [] letters = new int[26];
@@ -14,14 +14,36 @@ public class ScrambleStringJava {
             if (letters[i] != 0) return false;
         }
 
+        // real stuff, smaller problem
+        for (int i = 1; i < s1.length(); i++) {
+            // cut, next
+            if (
+                isScramble(
+                    s1.substring(0, i),
+                    s2.substring(0, i)
+                ) &&
+                isScramble(s1.substring(i), s2.substring(i))
+            ) return true;
+            // cut, switch, next
+            if (
+                isScramble(s1.substring(0, i),
+                           s2.substring(i)) &&
+                isScramble(s1.substring(i),
+                           s2.substring(0, i))
+            ) return true;
+        }
         return false;
     }
     
 
     public static void main(String[] args) {
         ScrambleStringJava test = new ScrambleStringJava();
-        System.out.println(test.isScramble("great", "rgeat"));
-        System.out.println(test.isScramble("abcde", "caebd"));
+        System.out.println(
+            test.isScramble("great", "rgeat")
+        );
+        System.out.println(
+            test.isScramble("abcde", "caebd")
+        );
         System.out.println(test.isScramble("a", "a"));
     }
 }
