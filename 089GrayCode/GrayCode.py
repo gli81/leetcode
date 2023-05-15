b3f4449824849445dcd0b9981a60aecdd4a08321
LGZ <liguangzheng998@hotmail.com>
2023-05-15 08:13:19 +0800

    Started 089;

---
+++ 089GrayCode/GrayCode.py
@@ -0,0 +1,16 @@
+# -*- coding: utf -8-*-
+
+from typing import List
+
+class GrayCode:
+    def grayCode(self, n: "int") -> "List[int]":
+        pass
+
+
+def main():
+    test = GrayCode()
+    print(test.grayCode(1))
+    print(test.grayCode(2))
+
+if __name__ == "__main__":
+    main()
---
+++ 089GrayCode/GrayCodeJava.java
@@ -0,0 +1,16 @@
+
+
+import java.util.List;
+
+public class GrayCodeJava {
+    public List<Integer> grayCode(int n) {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        GrayCodeJava test = new GrayCodeJava();
+        System.out.println(test.grayCode(2));
+        System.out.println(test.grayCode(1));
+    }
+}
