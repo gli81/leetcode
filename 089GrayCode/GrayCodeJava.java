1c7d3ce38e897899e21896f24f0a28364fdb7344
LGZ <liguangzheng998@hotmail.com>
2023-05-18 00:28:00 +0800

    Completed 089 java

--- 089GrayCode/GrayCodeJava.java
+++ 089GrayCode/GrayCodeJava.java
@@ -1,10 +1,18 @@
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GrayCodeJava {
     public List<Integer> grayCode(int n) {
-        return null;
+        List<Integer> ans = new ArrayList<>();
+        ans.add(0); // ans for n = 0
+        for (int i = 1; i <= n; i++) {
+            for (int j = ans.size() - 1; j > -1; j--) {
+                ans.add(ans.get(j) + (1 << (i - 1)));
+            }
+        }
+        return ans;
     }
 
 
@@ -12,5 +20,8 @@ public class GrayCodeJava {
         GrayCodeJava test = new GrayCodeJava();
         System.out.println(test.grayCode(2));
         System.out.println(test.grayCode(1));
+        System.out.println(test.grayCode(0));
     }
 }
+
+// 
