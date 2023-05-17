b15934af51df24b14ebdf7dbc60fcab184d89bf3
LGZ <liguangzheng998@hotmail.com>
2023-05-18 00:30:42 +0800

    Completed 089

--- 089GrayCode/GrayCode.py
+++ 089GrayCode/GrayCode.py
@@ -4,7 +4,11 @@ from typing import List
 
 class GrayCode:
     def grayCode(self, n: "int") -> "List[int]":
-        pass
+        ans = [0] ## ans for n = 0
+        for i in range(n):
+            for j in range(len(ans) - 1, -1, -1):
+                ans.append(ans[j] + (1 << (i)))
+        return ans
 
 
 def main():
