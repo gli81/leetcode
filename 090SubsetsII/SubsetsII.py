5280da0729195e12b20df5e23db0bfbe0ab2087b
LGZ <liguangzheng998@hotmail.com>
2023-05-18 00:36:16 +0800

    Started 090

---
+++ 090SubsetsII/SubsetsII.py
@@ -0,0 +1,17 @@
+# -*- coding: utf -8-*-
+
+from typing import List
+
+class SubsetsII:
+    def subsetsWithDup(self,
+                    nums: "List[int]") -> "List[List[int]]":
+        pass
+
+
+def main():
+    test = SubsetsII()
+    print(test.subsetsWithDup([1, 2, 2]))
+    print(test.subsetsWithDup([0]))
+
+if __name__ == "__main__":
+    main()
---
+++ 090SubsetsII/SubsetsIIJava.java
@@ -0,0 +1,21 @@
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SubsetsIIJava {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        return null;
+    }
+    
+
+    public static void main(String[] args) {
+        SubsetsIIJava test = new SubsetsIIJava();
+        System.out.println(
+            test.subsetsWithDup(new int[]{1, 2, 2})
+        );
+        System.out.println(
+            test.subsetsWithDup(new int[]{0})
+        );
+    }
+}
