59f4a4eb2bec0b31d464a3d74cd44d4ee4596ce9
LGZ <liguangzheng998@hotmail.com>
2023-05-18 10:44:19 +0800

    reformat

--- 016ThreeSumClosest/ThreeSumClosestJava.java
+++ 016ThreeSumClosest/ThreeSumClosestJava.java
@@ -4,8 +4,13 @@ import java.util.Arrays;
 
 public class ThreeSumClosestJava {
     public static void main(String[] args) {
-        System.out.println(new ThreeSumClosestJava().threeSumClosest2(new int[]{-1,2,1,-4}, 1));
-        System.out.println(new ThreeSumClosestJava().threeSumClosest2(new int[]{0, 0, 0}, 1));
+        ThreeSumClosestJava test = new ThreeSumClosestJava();
+        System.out.println(
+            test.threeSumClosest2(new int[]{-1,2,1,-4}, 1)
+        );
+        System.out.println(
+            test.threeSumClosest2(new int[]{0, 0, 0}, 1)
+        );
     }
 
     public int threeSumClosest2(int[] nums, int target) {
@@ -14,14 +19,20 @@ public class ThreeSumClosestJava {
         for (int i = 0; i < nums.length - 2; i++) {
             int lo = i + 1, hi = nums.length - 1;
             while(lo < hi) {
-                if (Math.abs(nums[i] + nums[lo] + nums[hi] - target) < diff) {
+                if (Math.abs(
+                    nums[i] + nums[lo] + nums[hi] - target
+                ) < diff) {
                     ans = nums[i] + nums[lo] + nums [hi];
-                    diff = Math.abs(nums[i] + nums[lo] + nums[hi] - target);
+                    diff = Math.abs(
+                        nums[i] + nums[lo] + nums[hi] - target
+                    );
                 }
                 if (diff == 0) return ans;
                 if (nums[i] + nums[lo] + nums[hi] > target) {
                     hi--;
-                } else if (nums[i] + nums[lo] + nums[hi] < target) {
+                } else if (
+                    nums[i] + nums[lo] + nums[hi] < target
+                ) {
                     lo++;
                 }
             }
