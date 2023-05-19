ed77c3a7a702a17418a8a63b373ca20f334e02ba
LGZ <liguangzheng998@hotmail.com>
2023-05-19 08:40:06 +0800

    Completed 090 py

--- 090SubsetsII/SubsetsII.py
+++ 090SubsetsII/SubsetsII.py
@@ -7,14 +7,26 @@ class SubsetsII:
                        nums: "List[int]") -> "List[List[int]]":
         ans = []
         nums.sort()
-        self.__subsets_helper(nums, [], ans)
+        self.__subsets_helper(0, nums, [], ans)
         return ans
 
     def __subsets_helper(self,
+                         start: "int",
                          nums: "List[int]",
                          cur: "List[int]",
                          ans: "List[List[int]]") -> "None":
-        pass
+        ans.append(cur[:])
+        for i in range(start, len(nums)):
+            ## if current element same as the last one
+            ## skip it
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            cur.append(nums[i])
+            self.__subsets_helper(i + 1,
+                                    nums,
+                                    cur,
+                                    ans)
+            cur.pop()
 
 
 def main():
