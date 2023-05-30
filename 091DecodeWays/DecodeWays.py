4b6aea423f25d8d197440401b682dba37cdddf90
LGZ <liguangzheng998@hotmail.com>
2023-05-31 00:04:01 +0800

    Started 091

---
+++ 091DecodeWays/DecodeWays.py
@@ -0,0 +1,15 @@
+# -*- coding: utf -8-*-
+
+class DecodeWays:
+    def numDecodings(self, s: "str") -> "int":
+        pass
+
+
+def main():
+    test = DecodeWays()
+    print(test.numDecodings("12"))
+    print(test.numDecodings("226"))
+    print(test.numDecodings("06"))
+
+if __name__ == "__main__":
+    main()
---
+++ 091DecodeWays/DecodeWaysJava.java
@@ -0,0 +1,15 @@
+
+
+public class DecodeWaysJava {
+    public int numDecodings(String s) {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        DecodeWaysJava test = new DecodeWaysJava();
+        System.out.println(test.numDecodings("12"));
+        System.out.println(test.numDecodings("226"));
+        System.out.println(test.numDecodings("06"));
+    }
+}
