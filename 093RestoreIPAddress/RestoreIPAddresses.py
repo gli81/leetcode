c6ad349581c40c4806f33f8c408473b1359d6648
LGZ <liguangzheng998@hotmail.com>
2023-06-05 09:30:05 +0800

    Started 093

---
+++ 093RestoreIPAddress/RestoreIPAddresses.py
@@ -0,0 +1,17 @@
+# -*- coding: utf -8-*-
+
+from typing import List
+
+class RestoreIPAddresses:
+    def restoreIpAddresses(self, s: "str") -> "List[str]":
+        pass
+
+
+def main():
+    test = RestoreIPAddresses()
+    print(test.restoreIpAddresses("25525511135"))
+    print(test.restoreIpAddresses("0000"))
+    print(test.restoreIpAddresses("101023"))
+
+if __name__ == "__main__":
+    main()
---
+++ 093RestoreIPAddress/RestoreIPAddressesJava.java
@@ -0,0 +1,22 @@
+
+
+import java.util.List;
+
+public class RestoreIPAddressesJava {
+    public List<String> restoreIpAddresses(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        RestoreIPAddressesJava test = new RestoreIPAddressesJava();
+        System.out.println(
+            test.restoreIpAddresses("25525511135")
+        );
+        System.out.println(
+            test.restoreIpAddresses("0000")
+        );
+        System.out.println(
+            test.restoreIpAddresses("101023")
+        );
+    }
+}
