450cd92422020738ce388a515bf71555af783688
LGZ <liguangzheng998@hotmail.com>
2023-08-03 00:30:50 -0400

    Worked on 121 Java

--- 121BestTimetoBuyandSellStock/BestTimetoBuyandSellStockJava.java
+++ 121BestTimetoBuyandSellStock/BestTimetoBuyandSellStockJava.java
@@ -1,18 +1,43 @@
 
 
 public class BestTimetoBuyandSellStockJava {
-    public int maxProfit(int[] prices) {
-        return 0;
+    /** brute force solution
+     * exceeds time limit
+     */
+    public int maxProfit1(int[] prices) {
+        int ans = 0;
+        // brute force
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] - prices[i] > ans) {
+                    ans = prices[j] - prices[i];
+                }
+            }
+        }
+        return ans;
     }
 
-    
+    public int maxProfit2(int[] prices) {
+        int ans = 0;
+        // brute force
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] - prices[i] > ans) {
+                    ans = prices[j] - prices[i];
+                }
+            }
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         BestTimetoBuyandSellStockJava test =
             new BestTimetoBuyandSellStockJava();
-        System.out.println(test.maxProfit(
+        System.out.println(test.maxProfit2(
             new int[]{7, 1, 5, 3, 6, 4}
         ));
-        System.out.println(test.maxProfit(
+        System.out.println(test.maxProfit2(
             new int[]{7, 6, 4, 3, 1}
         ));
     }
