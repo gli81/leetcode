92ba0963d13ebc1857b4bba42fda2264325902f8
LGZ <liguangzheng998@hotmail.com>
2023-08-16 21:46:07 -0400

    Completed 238

---
+++ 238ProductofArrayExceptSelf/ProductofArrayExceptSelf.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class ProductofArrayExceptSelf:
+    def productExceptSelf(self, nums: "List[int]") -> "List[int]":
+        left = [1 for _ in range(len(nums))]
+        right = [1] * len(nums)
+        ans = [1] * len(nums)
+        for i in range(1, len(nums)):
+            left[i] = left[i - 1] * nums[i - 1]
+            right[len(nums) - i - 1] = right[len(nums) - i] * nums[len(nums) - i]
+        for i in range(len(nums)):
+            ans[i] = left[i] * right[i]
+        return ans
+
+
+def main():
+    test = ProductofArrayExceptSelf()
+    print(test.productExceptSelf([1, 2, 3, 4]))
+    print(test.productExceptSelf([-1, 1, 0, -3, 3]))
+
+if __name__ == "__main__":
+    main()
---
+++ 238ProductofArrayExceptSelf/ProductofArrayExceptSelfJava.java
@@ -0,0 +1,38 @@
+
+import java.util.Arrays;
+
+public class ProductofArrayExceptSelfJava {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        left[0] = 1;
+        right[len - 1] = 1;
+        for (int i = 1; i < len; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+            right[len - i - 1] = right[len - i]  * nums[len - i];
+        }
+        // System.out.println(Arrays.toString(left));
+        // System.out.println(Arrays.toString(right));
+        int[] ans = new int [len];
+        for (int i = 0; i < len; i++) {
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
+
+    
+    public static void main(String[] args) {
+        ProductofArrayExceptSelfJava test = new ProductofArrayExceptSelfJava();
+        System.out.println(
+            Arrays.toString(
+                test.productExceptSelf(new int[]{1, 2, 3, 4})
+            )
+        );
+        System.out.println(
+            Arrays.toString(
+                test.productExceptSelf(new int[]{-1, 1, 0, -3, 3})
+            )
+        );
+    }
+}
