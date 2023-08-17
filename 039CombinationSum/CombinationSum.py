d85edd6b6e1470babcb1d8f55e65b8e9635e3c37
LGZ <liguangzheng998@hotmail.com>
2023-08-17 02:59:53 -0400

    Reformatted 039

--- 039CombinationSum/CombinationSum.py
+++ 039CombinationSum/CombinationSum.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
 class CombinationSum:
-    def combinationSum(self, candidates: "list[int]", target: "int") -> "list[list[int]]":
+    def combinationSum(self, candidates: "list[int]",
+                       target: "int")\
+                        -> "list[list[int]]":
         ans = []
         self.combSum_(ans, candidates, [], target, 0)
         return ans
     
-    def combSum_(self, ans: "list[list[int]]", candidates: "list[int]", current: "list[int]", remain: "int", start: "int"):
+    def combSum_(self, ans: "list[list[int]]",
+                 candidates: "list[int]",
+                 current: "list[int]",
+                 remain: "int",
+                 start: "int") -> "None":
         if remain < 0: return
         elif remain == 0: ans.append(current[:])
         else:
@@ -14,6 +20,7 @@ class CombinationSum:
                 current.append(candidates[i])
                 self.combSum_(ans, candidates, current, remain - candidates[i], i)
                 current.pop()
+
 
 def main():
     test = CombinationSum()
