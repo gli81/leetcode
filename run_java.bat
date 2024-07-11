629ee8ae76956524961348f97f2f9e100a3dfadf
LGZ <liguangzheng998@hotmail.com>
2024-07-11 18:19:08 -0400

    worked on run_java script

--- run_java.bat
+++ run_java.bat
@@ -1,2 +1,36 @@
 @echo off
-echo %cd%
+set "q=%1"
+
+:: check if there is input
+if "%q%"=="" (
+    echo No Input
+    exit /b
+)
+
+:: check if input is digit only
+echo %q%| findstr /r /x "[0-9][0-9]*" >nul
+if not %errorlevel% == 0 (
+    echo Not a Number
+    exit /b
+)
+
+setlocal enabledelayedexpansion
+set "found=false"
+
+for /d %%d in (%q%*) do (
+    if '!found!'=='false' (
+        cd %%d
+        set "found=true"
+    )
+)
+
+
+if '!found!'=='false' (
+    echo Question Not Found
+) else (
+    echo %cd%
+    javac -d ..\ -cp ..\ .\*.java
+    cd ..\
+    java
+)
+endlocal
