b5cc0a4596bd6a48db52cb1869d081405ecea856
LGZ <liguangzheng998@hotmail.com>
2024-09-06 06:54:51 -0400

    143 go, String for ListNode go

--- 143ReorderLinkedList/ReorderLinkedList.go
+++ 143ReorderLinkedList/ReorderLinkedList.go
@@ -5,8 +5,52 @@ import (
 	nodestructure "leetcode/node_structure"
 )
 
-func reorderList(head *nodestructure.ListNode) {}
+func reorderList(head *nodestructure.ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+	// split in two
+	l := head
+	r := head
+	for r.Next != nil && r.Next.Next != nil {
+		l = l.Next
+		r = r.Next.Next
+	}
+	l = l.Next
+	// reverse second half
+	var cur *nodestructure.ListNode
+	var tmp *nodestructure.ListNode
+	if l.Next != nil {
+		cur = l.Next
+		l.Next = nil
+		for cur.Next != nil {
+			tmp = cur.Next
+			cur.Next = l
+			l = cur
+			cur = tmp
+		}
+		cur.Next = l
+	} else {
+		cur = l
+	}
+	// merge two halves
+	ans := head
+	for cur.Next != nil {
+		tmp = ans.Next
+		ans.Next = cur
+		ans = tmp
+		tmp = cur.Next
+		cur.Next = ans
+		cur = tmp
+	}
+	tmp = ans.Next
+	ans.Next = cur
+	cur.Next = tmp
+	tmp.Next = nil
+}
 
 func main() {
-	fmt.Println("hihihi")
+	test1 := &nodestructure.ListNode{Val: 1, Next: nil}
+	reorderList(test1)
+	fmt.Println(test1)
 }
--- node_structure/ListNode.go
+++ node_structure/ListNode.go
@@ -1,6 +1,19 @@
 package nodestructure
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func (node *ListNode) String() string {
+	ans := fmt.Sprintf("[%d", node.Val)
+	cur := node
+	for cur.Next != nil {
+		ans = ans + fmt.Sprintf(", %d", cur.Next.Val)
+		cur = cur.Next
+	}
+	ans += "]"
+	return ans
 }
