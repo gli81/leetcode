c753a4dfe257642bc3941d03727b057bef5b6643
LGZ <liguangzheng998@hotmail.com>
2024-09-10 18:18:37 -0400

    worked on go TreeNode

--- node_structure/TreeNode.go
+++ node_structure/TreeNode.go
@@ -2,6 +2,8 @@
 
 package nodestructure
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -28,9 +30,28 @@ func (node *TreeNode) String() string {
 			level++
 		}
 	}
-	ans := "[" + node.Val
+	ans := fmt.Sprintf("[%d", node.Val)
+	q = append(q, node.Left)
+	q = append(q, node.Right)
+	cur_level := 1
+	for len(q) != 0 && cur_level < level {
+		qsize := len(q)
+		for i := 0; i < qsize; i++ {
+			cur, q = q[0], q[1:]
+			if nil == cur {
+				ans += ", nil"
+				q = append(q, nil, nil)
+			} else {
+				ans += fmt.Sprintf(", %d", cur.Val)
+				q = append(q, cur.Left, cur.Right)
+			}
+		}
+		cur_level++
+	}
+	ans += "]"
+	return ans
 }
 
 // func TreeNodeFromList(lst []int) *TreeNode {
-
+// TODO ask Drew kkkkk
 // }
