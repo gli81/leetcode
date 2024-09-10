8155250fa531483b71bf0586cc25514ff7426dfe
GLi81 <liguangzheng998@hotmail.com>
2024-09-10 17:05:23 -0400

    worked on go treenode

--- node_structure/TreeNode.go
+++ node_structure/TreeNode.go
@@ -8,9 +8,28 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// func (node *TreeNode) String() string {
-
-// }
+func (node *TreeNode) String() string {
+	q := make([]*TreeNode, 0) // queue
+	level := 0
+	if nil != node {
+		q = append(q, node)
+	}
+	var cur *TreeNode
+	for len(q) != 0 {
+		qsize := len(q)
+		for i := 0; i < qsize; i++ {
+			cur, q = q[0], q[1:]
+			if nil != cur.Left {
+				q = append(q, cur.Left)
+			}
+			if nil != cur.Right {
+				q = append(q, cur.Right)
+			}
+			level++
+		}
+	}
+	ans := "[" + node.Val
+}
 
 // func TreeNodeFromList(lst []int) *TreeNode {
 
