6d39424cb2af87ddca81a377edf5a71a16b1eae9
GLi81 <liguangzheng998@hotmail.com>
2024-09-10 16:27:36 -0400

    141 two pointers py java

--- 141LinkedListCycleDetection/LinkedListCycleDetection.py
+++ 141LinkedListCycleDetection/LinkedListCycleDetection.py
@@ -3,10 +3,10 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from node_structure import ListNode
+from node_structure.ListNode import ListNode
 
 class LinkedListCycleDetection:
-    def hasCycle(self, head: "ListNode") -> "bool":
+    def hasCycle1(self, head: "ListNode") -> "bool":
         s = set()
         cur = head
         while cur:
@@ -16,12 +16,30 @@ class LinkedListCycleDetection:
                 s.add(cur)
             cur = cur.next
         return False
+    
+    def hasCycle2(self, head: "ListNode") -> "bool":
+        """
+        two pointers
+        """
+        slow = fast = head
+        slow = slow.next
+        fast = fast.next
+        if not fast: return False
+        fast = fast.next
+        while slow and fast and fast.next and slow != fast:
+            slow = slow.next
+            fast = fast.next.next
+        if not fast or not fast.next: return False
+        return True
 
 
 def main():
     test = LinkedListCycleDetection()
-    print(test.hasCycle(
+    print(test.hasCycle2(
         ListNode.fromList([1, 2, 5, 3, 2])
+    ))
+    print(test.hasCycle2(
+        ListNode.fromList([1, 2, 3, 4, 5, 6, 7, 8])
     ))
 
 if __name__ == "__main__":
--- 141LinkedListCycleDetection/LinkedListCycleDetectionJava.java
+++ 141LinkedListCycleDetection/LinkedListCycleDetectionJava.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LinkedListCycleDetectionJava {
-    public boolean hasCycle(ListNode head) {
+    public boolean hasCycle1(ListNode head) {
         ListNode cur = head;
         Set<ListNode> s = new HashSet<>();
         while (null != cur) {
@@ -19,12 +19,30 @@ public class LinkedListCycleDetectionJava {
         return false;
     }
 
+    public boolean hasCycle2(ListNode head) {
+        ListNode slow, fast;
+        slow = fast = head;
+        slow = slow.next;
+        fast = fast.next;
+        if (null == fast) return false;
+        fast = fast.next;
+        while (null != slow && null != fast && null != fast.next && slow != fast) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (slow == fast) return true;
+        return false;
+    }
+
 
     public static void main(String[] args) {
         LinkedListCycleDetectionJava test =
             new LinkedListCycleDetectionJava();
+        // System.out.println(
+        //     test.hasCycle2(ListNode.fromList(new int[] {2, 5, 3, 7, 6}))
+        // );
         System.out.println(
-            test.hasCycle(ListNode.fromList(new int[] {2, 5, 3, 7, 6}))
+            test.hasCycle2(ListNode.fromList(new int[] {1}))
         );
     }
 }
