c38594658ec08d22a6c162618273f4a029df1e3f
LGZ <liguangzheng998@hotmail.com>
2024-10-05 00:08:05 -0400

    worked on 703

---
+++ 703KthLargestElementinaStream/KthLargestElementinaStream.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class KthLargestElementinaStream:
+    def __init__(self, k: "int", nums: "List[int]"):
+        pass
+
+    def add(self, val: "int") -> "int":
+        pass
---
+++ 703KthLargestElementinaStream/KthLargestElementinaStreamJava.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+
+public class KthLargestElementinaStreamJava {
+    private PriorityQueue<Integer> pq;
+    private int k;
+
+
+    public KthLargestElementinaStreamJava(int k, int[] nums) {
+        this.k = k;
+        this.pq = new PriorityQueue<>();
+        for (int num : nums) {
+            this.pq.add(num);
+            if (this.pq.size() > this.k) {
+                this.pq.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        this.pq.peek();
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
