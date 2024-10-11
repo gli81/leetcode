adecf195b65f50bffc4361d9e679d6d9da9d7be1
GLi81 <liguangzheng998@hotmail.com>
2024-10-11 00:40:50 -0400

    973 py java

--- 973KClosestPointstoOrigin/KClosestPointstoOrigin.py
+++ 973KClosestPointstoOrigin/KClosestPointstoOrigin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from typing import List
+import heapq
 
 class KClosestPointstoOrigin:
     def kClosest(
@@ -8,11 +9,33 @@ class KClosestPointstoOrigin:
         points: "List[List[int]]",
         k: "int"
     ) -> "List[List[int]]":
-        pass
+        pq = []
+        for p in points:
+            # print((p[0]**2 + p[1]**2)**0.5)
+            heapq.heappush(pq, ((p[0]**2 + p[1]**2)**0.5, p))
+        ans = []
+        for _ in range(k):
+            ans.append(heapq.heappop(pq)[1])
+        return ans
 
 
 def main():
     test = KClosestPointstoOrigin()
+    print(test.kClosest(
+        [
+            [0, 2],
+            [2, 2]
+        ],
+        1
+    ))
+    print(test.kClosest(
+        [
+            [3, 3],
+            [5, -1],
+            [-2, 4]
+        ],
+        2
+    ))
 
 if __name__ == "__main__":
     main()
--- 973KClosestPointstoOrigin/KClosestPointstoOriginJava.java
+++ 973KClosestPointstoOrigin/KClosestPointstoOriginJava.java
@@ -1,22 +1,47 @@
-
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KClosestPointstoOriginJava {
     public int[][] kClosest(int[][] points, int k) {
-        return null;
+        PriorityQueue<double[]> pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (a[0] > b[0]) return 1;
+                else if (a[0] == b[0]) return 0;
+                else return -1;
+            }
+        );
+        for (int[] p : points) {
+            pq.add(
+                new double[]{
+                    Math.sqrt((double)p[0] * p[0] + p[1] * p[1]),
+                    (double)p[0],
+                    (double)p[1]
+                }
+            );
+        }
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; ++i) {
+            double[] cur = pq.poll();
+            ans[i][0] = (int)cur[1];
+            ans[i][1] = (int)cur[2];
+        }
+        return ans;
     }
 
 
     public static void main(String[] args) {
         KClosestPointstoOriginJava test =
             new KClosestPointstoOriginJava();
-        System.out.println(
-            test.kClosest(
+        for (
+            int[] r :test.kClosest(
                 new int[][]{
                     {0, 2},
                     {2, 2}
                 },
                 1    
             )
-        );
+        ) {
+            System.out.println(Arrays.toString(r));
+        }
     }
 }
