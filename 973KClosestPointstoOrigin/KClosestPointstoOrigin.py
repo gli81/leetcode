56b58d56deefcc064005f160d9c3ae25ef5e7899
LGZ <liguangzheng998@hotmail.com>
2024-10-10 23:28:13 -0400

    started 973

---
+++ 973KClosestPointstoOrigin/KClosestPointstoOrigin.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class KClosestPointstoOrigin:
+    def kClosest(
+        self,
+        points: "List[List[int]]",
+        k: "int"
+    ) -> "List[List[int]]":
+        pass
+
+
+def main():
+    test = KClosestPointstoOrigin()
+
+if __name__ == "__main__":
+    main()
---
+++ 973KClosestPointstoOrigin/KClosestPointstoOriginJava.java
@@ -0,0 +1,22 @@
+
+
+public class KClosestPointstoOriginJava {
+    public int[][] kClosest(int[][] points, int k) {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        KClosestPointstoOriginJava test =
+            new KClosestPointstoOriginJava();
+        System.out.println(
+            test.kClosest(
+                new int[][]{
+                    {0, 2},
+                    {2, 2}
+                },
+                1    
+            )
+        );
+    }
+}
