0ede2948d71f3b627a26f50dd64d82eac20c42e1
GLi81 <liguangzheng998@hotmail.com>
2024-10-12 12:30:36 -0400

    work 621:

--- 621TaskScheduler/TaskScheduler.py
+++ 621TaskScheduler/TaskScheduler.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from typing import List
+import heapq
 
 class TaskScheduler:
     def leastInterval(self, tasks: "List[str]", n: "int") -> "int":
-        pass
+        ct = {}
+        for task in tasks:
+            ct[task] = ct.get(task, 0) + 1
+        pq = [-num for num in ct.values()]
+        heapq.heapify(pq)
+        q = []
+        time = 0
+        while len(pq) != 0 or len(q) != 0:
+            if len(q) != 0 and q[0][1] <= time:
+                heapq.heappush(pq, -q.pop(0)[0])
+            if len(pq) != 0:
+                cur = heapq.heappop(pq) + 1
+                q.append([cur, time + n + 1])
+            time += 1
+        return time
 
 
 def main():
     test = TaskScheduler()
+    print(test.leastInterval(['A', 'B', 'A', 'A', 'B', 'B'], 2))
 
 if __name__ == "__main__":
     main()
--- 621TaskScheduler/TaskSchedulerJava.java
+++ 621TaskScheduler/TaskSchedulerJava.java
@@ -1,13 +1,47 @@
 
 
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+
 public class TaskSchedulerJava {
-    public int leastInterval(String[] tasks, int n) {
-        
-        return 0;
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> ct = new HashMap<>();
+        for (char t : tasks) {
+            ct.put(t, ct.getOrDefault(t, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int v : ct.values()) {
+            pq.offer(v);
+        }
+        // System.out.println(pq);
+        int time = 0;
+        Deque<int[]> q = new ArrayDeque<>();
+        while (!pq.isEmpty() || !q.isEmpty()) {
+            if (!q.isEmpty() && time >= q.peek()[1]) {
+                pq.offer(q.poll()[0]);
+            }
+            if (!pq.isEmpty()) {
+                int cur = pq.poll() - 1;
+                if (cur > 0) {
+                    q.add(new int[]{cur, time + n + 1});
+                }
+            }
+            time++;
+        }
+        return time;
     }
 
 
     public static void main(String[] args) {
-        
+        TaskSchedulerJava test = new TaskSchedulerJava();
+        System.out.println(
+            test.leastInterval(
+                new char[]{'A', 'B', 'A', 'A', 'B', 'B'},
+                2
+            )
+        );
     }
 }
