5e2ca03f05de1d88687582979af1cdb97c22f8dc
LGZ <liguangzheng998@hotmail.com>
2024-10-12 01:10:41 -0400

    215 py java

--- 215KthLargestElementinanArray/KthLargestElementinanArray.py
+++ 215KthLargestElementinanArray/KthLargestElementinanArray.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from typing import List
+import heapq
 
 class KthLargestElementinanArray:
-    def findKthLargest(self, nums: "List[int]", k: "int") -> "int":
+    def findKthLargest1(self, nums: "List[int]", k: "int") -> "int":
+        nums.sort(reverse=True)
+        return nums[k - 1]
+
+    def findKthLargest2(self, nums: "List[int]", k: "int") -> "int":
         pass
+
+    def findKthLargest3(self, nums: "List[int]", k: "int") -> "int":
+        """
+        with PQ
+        """
+        pq = []
+        for num in nums:
+            if len(pq) == k and pq[0] < num or len(pq) < k:
+                heapq.heappush(pq, num)
+                if len(pq) > k:
+                    heapq.heappop(pq)
+        # print(pq)
+        return pq[0]
 
 
 def main():
     test = KthLargestElementinanArray()
+    print(test.findKthLargest1([2, 3, 1, 5, 4], 2))
+    print(test.findKthLargest3([2, 3, 1, 5, 4], 2))
 
 if __name__ == "__main__":
     main()
--- 215KthLargestElementinanArray/KthLargestElementinanArrayJava.java
+++ 215KthLargestElementinanArray/KthLargestElementinanArrayJava.java
@@ -1,12 +1,35 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthLargestElementinanArrayJava {
-    public int findKthLargest(int[] nums, int k) {
+    public int findKthLargest1(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
 
+    public int findKthLargest2(int[] nums, int k) {
         return 0;
+    }
+
+    public int findKthLargest3(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            if (pq.size() == k && pq.peek() < num || pq.size() < k) {
+                pq.offer(num);
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+        }
+        return pq.peek();
     }
 
 
     public static void main(String[] args) {
-        
+        KthLargestElementinanArrayJava test =
+            new KthLargestElementinanArrayJava();
+        System.out.println(
+            test.findKthLargest1(new int[]{2, 3, 1, 5, 4}, 2)
+        );
     }
 }
