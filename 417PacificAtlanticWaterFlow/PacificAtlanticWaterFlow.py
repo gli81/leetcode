3f828f0fa3b52f225489f1ace9dce0bd93664771
LGZ <liguangzheng998@hotmail.com>
2024-12-29 19:29:45 -0500

    complete 417 java py

--- 417PacificAtlanticWaterFlow/PacificAtlanticWaterFlow.py
+++ 417PacificAtlanticWaterFlow/PacificAtlanticWaterFlow.py
@@ -6,7 +6,49 @@ class PacificAtlanticWaterFlow:
     def pacificAtlantic(
         self, heights: "List[List[int]]"
     ) -> "List[List[int]]":
-        pass
+        ans = []
+        m = len(heights)
+        if not m: return ans
+        n = len(heights[0])
+        if not n: return ans
+        atl = [[False for _ in range(n)] for _ in range(m)]
+        pac = [[False for _ in range(n)] for _ in range(m)]
+        aq = []
+        pq = []
+        for i in range(m):
+            aq.append((i, n-1))
+            pq.append((i, 0))
+        for j in range(n):
+            aq.append((m-1, j))
+            pq.append((0, j))
+        dirs = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        while aq:
+            cur_x, cur_y = aq.pop()
+            atl[cur_x][cur_y] = True
+            for dx, dy in dirs:
+                new_x, new_y = cur_x + dx, cur_y + dy
+                if (
+                    0 <= new_x < m and 0 <= new_y < n
+                    and not atl[new_x][new_y]
+                    and heights[new_x][new_y] >= heights[cur_x][cur_y]
+                ):
+                    aq.append((new_x, new_y))
+        while pq:
+            cur_x, cur_y = pq.pop()
+            pac[cur_x][cur_y] = True
+            for dx, dy in dirs:
+                new_x, new_y = cur_x + dx, cur_y + dy
+                if (
+                    0 <= new_x < m and 0 <= new_y < n
+                    and not pac[new_x][new_y]
+                    and heights[new_x][new_y] >= heights[cur_x][cur_y]
+                ):
+                    pq.append((new_x, new_y))
+        for i in range(m):
+            for j in range(n):
+                if atl[i][j] and pac[i][j]:
+                    ans.append([i, j])
+        return ans
 
 
 def main():
--- 417PacificAtlanticWaterFlow/PacificAtlanticWaterFlowJava.java
+++ 417PacificAtlanticWaterFlow/PacificAtlanticWaterFlowJava.java
@@ -1,10 +1,108 @@
 
 
 import java.util.List;
+import java.util.Queue;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class PacificAtlanticWaterFlowJava {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        return null;
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        int m, n;
+        m = heights.length;
+        if (m == 0) return ans;
+        n = heights[0].length;
+        if (n == 0) return ans;
+        boolean[][] atl = new boolean[m][n];
+        boolean[][] pac = new boolean[m][n];
+        // initialize queues
+        Queue<int[]> aq = new LinkedList<>();
+        Queue<int[]> pq = new LinkedList<>();
+        for (int i = 0; i < m; ++i) {
+            aq.offer(new int[]{i, n-1});
+            pq.offer(new int[]{i, 0});
+        }
+        for (int j = 0; j < n; ++j) {
+            aq.offer(new int[]{m-1, j});
+            pq.offer(new int[]{0, j});
+        }
+        int[][] dirs = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
+        while (!aq.isEmpty()) {
+            int[] cur = aq.poll();
+            atl[cur[0]][cur[1]] = true;
+            for (int[] d : dirs) {
+                int newx = cur[0] + d[0];
+                int newy = cur[1] + d[1];
+                if (
+                    newx >= 0 && newx < m
+                    && newy >= 0 && newy < n
+                    && !atl[newx][newy]
+                    && heights[newx][newy] >= heights[cur[0]][cur[1]]
+                ) {
+                    aq.offer(new int[]{newx, newy});
+                }
+            }
+        }
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            pac[cur[0]][cur[1]] = true;
+            for (int[] d : dirs) {
+                int newx = cur[0] + d[0];
+                int newy = cur[1] + d[1];
+                if (
+                    newx >= 0 && newx < m
+                    && newy >= 0 && newy < n
+                    && !pac[newx][newy]
+                    && heights[newx][newy] >= heights[cur[0]][cur[1]]
+                ) {
+                    pq.offer(new int[]{newx, newy});
+                }
+            }
+        }
+        // for (int i = 0; i < m; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         System.out.print(atl[i][j]);
+        //         if (j != n - 1) {
+        //             System.out.print(", ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+        // for (int i = 0; i < m; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         System.out.print(pac[i][j]);
+        //         if (j != n - 1) {
+        //             System.out.print(", ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (atl[i][j] && pac[i][j]) {
+                    ans.add(Arrays.asList(i, j));
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        PacificAtlanticWaterFlowJava test = new PacificAtlanticWaterFlowJava();
+        System.out.println(test.pacificAtlantic(
+            new int[][] {
+                {4, 2, 7, 3, 4},
+                {7, 4, 6, 4, 7},
+                {6, 3, 5, 3, 6}
+            }
+        ));
     }
 }
