f3b6b6cf7a195e5a1113ab129e0472de4b4c02a7
LGZ <liguangzheng998@hotmail.com>
2025-06-16 16:14:42 -0400

    323 dsu py java

--- 323NumberofConnectedComponentsinanUndirectedGraph/NumberofConnectedComponentsinanUndirectedGraph.py
+++ 323NumberofConnectedComponentsinanUndirectedGraph/NumberofConnectedComponentsinanUndirectedGraph.py
@@ -27,12 +27,49 @@ class NumberofConnectedComponentsinanUndirectedGraph:
                 visited.add(i)
                 self.__dfs(adj,visited, i)
 
+    def countComponents3(self, n: "int", edges: "List[List[int]]") -> "int":
+        dsu = DSU(n)
+        for [a, b] in edges:
+            dsu.union(a, b)
+        return dsu.getParts()
+
+
+class DSU:
+    def __init__(self, n: "int"):
+        self.__parents = [i for i in range(n)]
+        self.__heights = [1 for _ in range(n)]
+        self.__parts = n
+
+    def union(self, a: "int", b: "int") -> "None":
+        ra = self.__find(a)
+        rb = self.__find(b)
+        if ra != rb:
+            if self.__heights[ra] < self.__heights[rb]:
+                self.__parents[ra] = rb
+            elif self.__heights[ra] > self.__heights[rb]:
+                self.__parents[rb] = ra
+            else:
+                self.__parents[rb] = ra
+                self.__heights[ra] += 1
+            self.__parts -= 1
+
+    def __find(self, n: "int") -> "int":
+        """
+        Find the root of a given node
+        """
+        if self.__parents[n] != n:
+            self.__parents[n] = self.__find(self.__parents[n])
+        return self.__parents[n]
+
+    def getParts(self) -> "int":
+        return self.__parts
+
 
 def main():
     test = NumberofConnectedComponentsinanUndirectedGraph()
-    print(test.countComponents1(3, [[0, 1], [0, 2]]))
-    print(test.countComponents1(5, [[0, 1], [1, 2], [3, 4]]))
-    print(test.countComponents1(6, [[0, 1], [1, 2], [2, 3], [4, 5]]))
+    print(test.countComponents3(3, [[0, 1], [0, 2]]))
+    print(test.countComponents3(5, [[0, 1], [1, 2], [3, 4]]))
+    print(test.countComponents3(6, [[0, 1], [1, 2], [2, 3], [4, 5]]))
 
 if __name__ == "__main__":
     main()
--- 323NumberofConnectedComponentsinanUndirectedGraph/NumberofConnectedComponentsinanUndirectedGraphJava.java
+++ 323NumberofConnectedComponentsinanUndirectedGraph/NumberofConnectedComponentsinanUndirectedGraphJava.java
@@ -29,4 +29,58 @@ public class NumberofConnectedComponentsinanUndirectedGraphJava {
             }
         }
     }
+
+    public int countComponents3(int n, int[][] edges) {
+        DSU dsu = new DSU(n);
+        for (int[] e : edges) {
+            dsu.union(e[0], e[1]);
+        }
+        return dsu.getParts();
+    }
+}
+
+
+class DSU {
+    private int[] parent;
+    private int[] heights;
+    private int count;
+
+
+    public DSU(int n) {
+        this.parent = new int[n];
+        this.heights = new int[n];
+        this.count = n;
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+            heights[i] = 1;
+        }
+    }
+
+
+    private int find(int n) {
+        if (parent[n] != n) {
+            parent[n] = find(parent[n]);
+        }
+        return parent[n];
+    }
+
+    public void union(int a, int b) {
+        int ra = find(a);
+        int rb = find(b);
+        if (ra != rb) {
+            if (heights[ra] < heights[rb]) {
+                parent[ra] = rb;
+            } else if (heights[ra] > heights[rb]) {
+                parent[rb] = ra;
+            } else {
+                parent[rb] = ra;
+                heights[ra]++;
+            }
+            this.count--;
+        }
+    }
+
+    public int getParts() {
+        return count;
+    }
 }
