2f8854d4336968e63225b7f666f5d18645786e67
GLi81 <liguangzheng998@hotmail.com>
2025-08-13 03:07:03 -0700

    oa prep, 647 py, 128 reformat

--- 128LongestConsecutiveSequence/LongestConsecutiveSequence.py
+++ 128LongestConsecutiveSequence/LongestConsecutiveSequence.py
@@ -3,34 +3,27 @@
 from typing import List
 
 class LongestConsecutiveSequence:
-    def longestConsecutive(self,
-                           nums: "List[int]") -> "int":
+    def longestConsecutive(self, nums: List[int]) -> int:
         ## 用一个set记录都有哪些数字
-        record = set()
-        for num in nums:
-            record.add(num)
-        maxx = 0
+        s = set(nums)
+        ans = 0
         ## 遍历nums，如果nums[i] - 1在set里
-        ## 那nums[i]已经在计算nums[i] - 1的interval时
-        ## 数过了
-        ## 如果不在
-        ## nums[i]开始找，尝试更新maxx
+        ## 那nums[i]已经在/将在计算nums[i] - 1的interval时数到
+        ## 如果不在 从nums[i]开始找，尝试更新max_
         for num in nums:
-            tryit = num
-            if tryit - 1 not in record:
-                count = 0
-                while tryit in record:
-                    count += 1
-                    tryit += 1
-                maxx = max(maxx, count)
-        return maxx
+            temp = num
+            if temp - 1 not in s:
+                ct = 0
+                while temp in s:
+                    ct += 1
+                    temp += 1
+                ans = max(ans, ct)
+        return ans
 
 
 def main():
     test = LongestConsecutiveSequence()
-    print(
-        test.longestConsecutive([100, 4, 200, 1, 3, 2])
-    )
+    print(test.longestConsecutive([100, 4, 200, 1, 3, 2]))
 
 if __name__ == "__main__":
     main()
